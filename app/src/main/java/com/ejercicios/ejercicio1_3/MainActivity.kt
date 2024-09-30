1b81bab837d8613f71bb5d3239aef8c9254a24b2
emiralles <eileen.miralles@iesnervion.es>
2024-09-30 13:08:53 +0200

    Login without password and exercise 2.1 done

--- app/src/main/java/com/ejercicios/ejercicio1_3/MainActivity.kt
+++ app/src/main/java/com/ejercicios/ejercicio1_3/MainActivity.kt
@@ -1,20 +1,87 @@
 package com.ejercicios.ejercicio1_3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.ContentView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    companion object {
+        lateinit var username: EditText
+        var contentView: Int = 0
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        changeContentView(R.layout.activity_main)
+        login()
+        Log.d(":::Vida", "Acabo de crear la aplicacion")
         }
+
+    private fun login() {
+        //Username
+        username = findViewById<EditText>(R.id.usernameEdit)
+        //Button
+        findViewById<Button>(R.id.enterButton)
+            .setOnClickListener {
+                changeContentView(R.layout.welcome_view)
+                findViewById<TextView>(R.id.welcomeText).append(username.text)
+            }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(":::Vida", "Acaba de empezar la aplicacion")
+        }
+    override fun onResume(){
+        super.onResume()
+        //Toasty
+        if (contentView == R.layout.welcome_view) {
+            val text = "Welcome Back, " + username.text
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(this, text, duration)
+            toast.show()
+        }
+        Log.d(":::Vida", "La aplicacion continua")
+        }
+    override fun onPause(){
+        super.onPause()
+        var lauraPausini = findViewById<LinearLayout>(R.id.PauseLayout)
+        lauraPausini.visibility = View.VISIBLE
+        findViewById<Button>(R.id.unpauseButton)
+            .setOnClickListener {
+                lauraPausini.visibility = View.GONE
+            }
+        Log.d(":::Vida", "Acabo de pausar la aplicacion")
+        }
+    override fun onStop(){
+        super.onStop()
+        val text = "Se ha cerrado la sesión de " + username.text
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(this, text, duration)
+        toast.show()
+        changeContentView(R.layout.activity_main)
+        login()
+        Log.d(":::Vida", "Acabo de parar la aplicacion")
+        }
+
+    private fun changeContentView(view: Int) {
+        setContentView(view)
+        contentView = view
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(":::Vida", "Acabo de DESTRUIR la aplicacion")
+        }
+
 }
